9e0165c0278916180b4b846a92175d55ecc7f2f5
RaphaelEsparta <83084343+RaphaelEsparta@users.noreply.github.com>
2022-05-14 17:49:54 -0300

    no message

---
+++ ListView/app/src/main/java/com/example/listviewapp/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.listviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> itens = new ArrayList<>();
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        loadItensFromFile();
+        updateListView();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String aux = itens.get(i);
+                aux = aux +" clicked";
+                itens.set(i, aux);
+                updateListView();
+                listView.requestFocusFromTouch();
+                listView.setSelection(i);
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                itens.remove(i);
+                updateListView();
+                if(i > 1){
+                    listView.requestFocusFromTouch();
+                    listView.setSelection(i - 1);
+                }
+
+                return true;
+            }
+        });
+    }
+
+    public void addButtonClick(View v){
+        counter++;
+        itens.add("Item" + counter);
+        updateListView();
+    }
+
+    void updateListView(){
+        //        tranforma o array em componente
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+        listView.setAdapter(adapter);
+        saveItensToFile();
+
+    }
+
+    void loadItensFromFile(){
+        try {
+            InputStream stream = this.openFileInput("Lista.txt");
+            InputStreamReader streamReader = new InputStreamReader(stream);
+            BufferedReader reader = new BufferedReader(streamReader);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                itens.add(line);
+            }
+            reader.close();
+            streamReader.close();
+            stream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    void saveItensToFile(){
+        try {
+            OutputStream stream = MainActivity.this.openFileOutput(
+                    "lista.txt",
+                    MODE_PRIVATE
+            );
+            OutputStreamWriter write = new OutputStreamWriter(stream);
+            for (String s: itens){
+                write.write(s + "\n");
+
+            }
+            write.flush();
+            write.close();
+            stream.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ agenda e save end load/app/src/main/java/com/example/agendaapp/controller/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.agendaapp.controller;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.agendaapp.R;
+import com.example.agendaapp.model.DataModel;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listview);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                goToDetailActivity(i);
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                DataModel.getInstance().contacts.remove(i);
+                updateListView();
+                if(i>1){
+                    listView.requestFocusFromTouch();
+                    listView.setSelection(i-1);
+                }
+                DataModel.getInstance().saveToFile(MainActivity.this);
+                return false;
+            }
+        });
+        DataModel.getInstance().loadFromFile(MainActivity.this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateListView();
+    }
+
+    void updateListView(){
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                DataModel.getInstance().getStringContacts()
+        );
+        listView.setAdapter(adapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return true;
+    }
+
+    void goToDetailActivity(int index){
+        Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+        intent.putExtra("index", index);
+        startActivity(intent);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.menu_add_contact){
+//            Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+//            startActivity(intent);
+            goToDetailActivity(-1);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
